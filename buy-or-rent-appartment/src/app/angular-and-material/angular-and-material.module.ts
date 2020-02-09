259f77dd2579f891b07837a0fb79221a14234c06
Siarhei Kuchuk <cuchuk.sergey@gmail.com>
2020-02-09 21:15:27 +0300

    #1 Upgrade to latest Angular - fixed

--- buy-or-rent-appartment/src/app/angular-and-material/angular-and-material.module.ts
+++ buy-or-rent-appartment/src/app/angular-and-material/angular-and-material.module.ts
@@ -3,20 +3,18 @@ import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { ReactiveFormsModule } from '@angular/forms';
 
-import {
-  MatButtonModule,
-  MatMenuModule,
-  MatIconModule,
-  MatCardModule,
-  MatSidenavModule,
-  MatFormFieldModule,
-  MatInputModule,
-  MatTooltipModule,
-  MatToolbarModule,
-  MatCommonModule,
-  MatListModule,
-  MatCheckboxModule,
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatCommonModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   imports: [
